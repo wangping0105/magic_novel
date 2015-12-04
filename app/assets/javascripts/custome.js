8d7ba46314f27765fcacff77dc2280fc465c58d3
jack ping <525399584@qq.com>
2015-12-04 15:56:05 +0800

    转载 作者名 1

---
+++ app/assets/javascripts/custome.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".form_reset").on('click', function(){
+        $(':input',$(this).parents("form")[0])
+            .not(':button, :submit, :reset, :hidden')
+            .val('')
+            .removeAttr('checked')
+            .removeAttr('selected');
+    })
+})
