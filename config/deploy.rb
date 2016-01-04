2607bbac3c358a39977dd9b0c5ec0bc7bc2d2f59
jack ping <525399584@qq.com>
2016-01-04 14:50:03 +0800

    add js file to vdender

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,7 +55,7 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-      # invoke 'unicorn:restart'
+      invoke 'unicorn:restart'
       # invoke 'unicorn:duplicate'
       # invoke 'unicorn:legacy_restart'
     end
