691e928178daf7d0ed98806bf7b6524c9706a524
jack ping <525399584@qq.com>
2018-04-05 00:55:58 +0800

    commend some

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user, repo, gist"
-end
+# use OmniAuth::Builder do
+#   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user, repo, gist"
+# end
