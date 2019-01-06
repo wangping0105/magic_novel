3d6e6abbab02ef690139e40f90a7a76408811b0e
jack ping <525399584@qq.com>
2019-01-07 00:39:35 +0800

    fix datetime

--- app/workers/eosknights_worker.rb
+++ app/workers/eosknights_worker.rb
@@ -2,7 +2,7 @@ class EosknightsWorker
   include Sidekiq::Worker
 
   def perform(set_time = nil)
-    set_time ||= Time.now.at_beginning_of_day
+    set_time ||= (Time.now.at_beginning_of_day - 10.hours)
     EosKnight.fetch_data(set_time)
   end
 end
