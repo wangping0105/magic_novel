60563a036d44757625d9ccc0ba6da9e307de528f
jack ping <525399584@qq.com>
2019-01-07 18:39:07 +0800

    fxi

--- app/workers/eosknights_worker.rb
+++ app/workers/eosknights_worker.rb
@@ -2,7 +2,7 @@ class EosknightsWorker
   include Sidekiq::Worker
 
   def perform(set_time = nil)
-    set_time ||= (Time.now.at_beginning_of_day - 10.hours)
+    set_time ||= (Time.now.at_beginning_of_day - 8.hours-5.minutes)
     EosKnight.fetch_data(set_time)
   end
 end
